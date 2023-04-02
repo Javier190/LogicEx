26e7ce84f745196653581503f695aa693956535f
Javier <jav.aravenac@gmail.com>
2023-04-02 12:04:38 -0400

    AddTwoNumbers Done! in class with ending text Again

--- src/AddTwoNumbersAgain.java
+++ src/AddTwoNumbersAgain.java
@@ -1,12 +1,8 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 //You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
 // and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 //You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-public class AddTwoNumbers {
+public class AddTwoNumbersAgain {
 
     public static void main(String[] args) {
 
@@ -20,43 +16,41 @@ public class AddTwoNumbers {
         ListNode nodeC = new ListNode(6, nodeD);
         ListNode l2 = new ListNode(5, nodeC);
 
-        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        AddTwoNumbersAgain addTwoNumbers = new AddTwoNumbersAgain();
         addTwoNumbers.addTwoNumbers(l1,l2);
 
     }
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
         int carry = 0;
-        ListNode dummy = new ListNode(0); // creating an dummy list
-        ListNode curr = dummy;
+        int remainder = 0;
+        ListNode listFinal = new ListNode(0);
 
         while (l1 != null || l2 != null){
 
             int sum = 0;
 
-            int test = 10;
-            System.out.println("sdasd" + test%5);
-
-            if (carry >= 10){
+            if (carry > 0){
                 sum = l1.val + l2.val + carry;
-
-                System.out.println("Valor suma con Reserva: " + sum);
+                remainder = sum/10;
             } else {
-                sum = l1.val + l2.val;
-                System.out.println("Valor de la suma es:" + sum);
 
-                if (sum >= 10){
-                    carry = sum - 10;
-                    System.out.println("ELSE de Suma es: " + sum);
-                }
+                sum = l1.val + l2.val;
+                remainder = sum/10;
             }
 
-                l1 = l1.next;
-                l2 = l2.next;
+            if (remainder > 0 ){
+                sum = remainder/10;
+                carry = remainder%10;
+            } else {
+                //aqui va algo?
+            }
+            listFinal.val = sum;
+            System.out.println(" *** El Nodo tiene el valor de *** " + listFinal.val);
 
-            System.out.println("Fin un ciclo");
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        return null;
+        return listFinal;
     }
 }
